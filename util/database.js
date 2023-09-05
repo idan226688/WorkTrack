162395e4c2159242efdf40ce7a135fe235a87588
idan226688 <105588777+idan226688@users.noreply.github.com>
2023-09-06 02:10:19 +0300

    pass

--- util/database.js
+++ util/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'yubV26111970',
+  password: '*',
   database: 'employees',
   waitForConnections: true,
   connectionLimit: 10,
