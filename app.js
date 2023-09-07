bdfd805df422e9a9e528305800d9b6d71f064e9b
idan226688 <105588777+idan226688@users.noreply.github.com>
2023-09-08 02:14:35 +0300

    c

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ loginRoute.setupLoginRoute(app)
 userRegistration.setupRegistrationRoute(app);
 SubmitRoute.setupSubmitRoute(app)
 
+app.use((req,res,next)=> {
+  res.status(404).send('404',"page not found")
+})
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
